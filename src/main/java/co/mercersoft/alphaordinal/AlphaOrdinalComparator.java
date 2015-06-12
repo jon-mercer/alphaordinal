1a8bc14b57c8e53b0d3521b5419b628f0bee1f54
Jon Mercer <jmercer@grubhub.com>
2015-06-12 14:32:48 -0500

    Implement the alpha ordinal comparator

--- src/main/java/co/mercersoft/alphaordinal/AlphaOrdinalComparator.java
+++ src/main/java/co/mercersoft/alphaordinal/AlphaOrdinalComparator.java
@@ -6,6 +6,10 @@ public class AlphaOrdinalComparator implements Comparator<Integer> {
 
     @Override
     public int compare(Integer o1, Integer o2) {
-        return 0;
+        NumberToWord numberToWord = new NumberToWord();
+        String word1 = numberToWord.convert(o1);
+        String word2 = numberToWord.convert(o2);
+
+        return word1.compareTo(word2);
     }
 }
---
+++ src/test/java/co/mercersoft/alphaordinal/AlphaOrdinalComparatorTest.groovy
@@ -0,0 +1,36 @@
+package co.mercersoft.alphaordinal
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class AlphaOrdinalComparatorTest extends Specification {
+
+    @Unroll
+    def "AlphaOrdinalComparator should return #o1 greater than #o2"(){
+        given:
+        def comparator = new AlphaOrdinalComparator()
+
+        expect:
+        comparator.compare(o1, o2) > 0
+
+        where:
+        o1     | o2
+        2      | 1010
+        1      | 8
+        0      | 5
+        200    | 500
+    }
+
+    def "AlphaOrderinalComparator should sort the numbers 0-9"() {
+
+        given:
+        def numbers = new ArrayList<Integer>()
+        numbers.addAll([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+        when:
+        Collections.sort(numbers, new AlphaOrdinalComparator())
+
+        then:
+        numbers == [8, 5, 4, 9, 1, 7, 6, 3, 2, 0]
+    }
+}
